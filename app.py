ad523ae05ddcbc9ccdd15ce88eb55b773c9ce68a
Lila Mullany <lilamullany@gmail.com>
2022-01-27 12:13:26 -0800

    updated application and model files to be current

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html 
+from dash import dcc
+from dash import html
+from dash import dash_table
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-import dash_table
-from dash.exceptions import PreventUpdate
-import plotly.graph_objects as go
 
 from flask import Flask
-from flask import render_template, request, Response
+from flask import render_template, Response
 
 import pandas as pd
 
@@ -19,20 +16,23 @@ import time
 
 # edgeIQ
 camera = edgeiq.WebcamVideoStream(cam=0)
-obj_detect = edgeiq.ObjectDetection("alwaysai/mobilenet_ssd")
+obj_detect = edgeiq.ObjectDetection("alwaysai/ssd_mobilenet_v1_coco_2018_01_28")
 obj_detect.load(engine=edgeiq.Engine.DNN)
 
 # Data
 data = pd.DataFrame()
 START_TIME = time.time()
 
+
 # functions for rendering frame and performing object detection
 def gen_video_feed():
     while True:
         frame = camera.read()
-        frame = perform_object_detection(frame)
+        if frame is not None:
+            frame = perform_object_detection(frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 def perform_object_detection(frame):
     """Perform object detction on an image, update
@@ -44,30 +44,33 @@ def perform_object_detection(frame):
     Returns:
         string: The string representation of the image
     """
-    frame = edgeiq.resize(frame, width=800, height=300)
-    results = obj_detect.detect_objects(frame, confidence_level=.5)
-    frame = edgeiq.markup_image(
-            frame, results.predictions, colors=obj_detect.colors)
-    frame = cv2.imencode('.jpg', frame)[1].tobytes()
+    if frame is not None:
+        results = obj_detect.detect_objects(frame, confidence_level=.5)
+        frame = edgeiq.markup_image(
+                frame, results.predictions, colors=obj_detect.colors)
+        frame = edgeiq.resize(frame, width=800, height=300)
+        frame = cv2.imencode('.jpg', frame)[1].tobytes()
 
-    # update data for table 
-    objects = {
-        'timestamp': str(round((time.time() - START_TIME), 0)),
-        'labels': ", ".join([p.label for p in results.predictions])
-    }
+        # update data for table
+        objects = {
+            'timestamp': str(round((time.time() - START_TIME), 0)),
+            'labels': ", ".join([p.label for p in results.predictions])
+        }
 
-    global data
-    if data is None:
-        data = pd.DataFrame({k: [v] for k, v in objects.items()})
-    else:
-        data = data.append(pd.DataFrame({k: [v] for k, v in objects.items()}))
-    
-    data = data.drop_duplicates()
+        global data
+        if data is None:
+            data = pd.DataFrame({k: [v] for k, v in objects.items()})
+        else:
+            data = data.append(pd.DataFrame({k: [v] for k, v in objects.items()}))
+
+        data = data.drop_duplicates()  
 
     return frame
 
+
 # Flask app
 app = Flask(__name__, instance_relative_config=False)
+
 
 # Flask routes (add as needed)
 @app.route('/video_feed')
@@ -75,14 +78,16 @@ def video_feed():
     return Response(gen_video_feed(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 # Dash Setup
 dash_app = dash.Dash(
     __name__,
-    server=app, # associate Flask
+    server=app,  # associate Flask
     assets_folder="./static",
     url_base_pathname='/dash/',
     external_stylesheets=[dbc.themes.LUX]
@@ -106,7 +111,7 @@ dash_app.layout = dbc.Container(fluid=True, children=[
         columns=[],
         style_as_list_view=False,
         page_action="native",
-        page_size= 10,
+        page_size=10,
         export_format="csv",
         style_header={
             'backgroundColor': 'rgba(0,0,0,0.2)',
@@ -126,15 +131,15 @@ dash_app.layout = dbc.Container(fluid=True, children=[
         sort_by={
             'column_id': 'timestamp',
             'direction': 'desc'
-        })
-    ,
+        }),
     # automatically update periodically
     dcc.Interval(
         id='interval-component',
-        interval=1*5000, # in milliseconds
+        interval=1*5000,  # in milliseconds
         n_intervals=0
     )
 ])
+
 
 # Dash Callbacks
 @dash_app.callback(
